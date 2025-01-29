3431a133949753dcbb52b8b11a858fdf1fc0f126
Rahul Kumar <rahulmob.3607@gmail.com>
2025-01-29 22:40:44 +0530

    added basic server code

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const PORT = 8000;
+
+const server = http.createServer((req,res) => {
+     const {method , url} = req;
+    if(url==='/' && method === "GET"){
+        res.end("hello from server");
+    }
+    if(url==='/about' && method === "GET"){
+        res.end("hello from about");
+    }
+    if(url==='/data' && method === "POST"){
+        let body ='';
+        req.on('data', chunk =>{
+            body += chunk.toString();
+        });
+
+        req.on('end',()=>{
+            res.end(JSON.stringify({message : "data recieved " , data: body}));
+        })
+    }
+});
+
+
+server.listen(PORT , () => {
+    console.log("Listening on port http://localhost:8080");
+})
+
